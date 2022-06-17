2fce8ce574fd68e167c953a0e3d72c14874a5631
Atabek <44347973+ikays97@users.noreply.github.com>
2022-06-18 01:50:59 +0500

    home gadget types

--- src/menu-category/menu-category.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('menu-category')
-export class MenuCategoryController {}
--- src/menu-category/menu-category.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MenuCategoryService } from './menu-category.service';
-import { MenuCategoryController } from './menu-category.controller';
-
-@Module({
-  providers: [MenuCategoryService],
-  controllers: [MenuCategoryController]
-})
-export class MenuCategoryModule {}
