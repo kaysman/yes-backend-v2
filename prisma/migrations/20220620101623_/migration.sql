cd7b695c1fa3a9c41d5e889ed531569dce555813
Hajymyrat <hajymyratayydov@gmail.com>
2022-06-20 18:11:57 +0500

    serve static files

---
+++ prisma/migrations/20220620101623_/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "HomeGadget_queue_key";
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as bodyParser from 'body-parser';
+import { join } from 'path';
 
 import {
   ClassSerializerInterceptor,
@@ -8,15 +9,19 @@ import {
   NestFactory,
   Reflector,
 } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
   app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(3333);
+  await app.listen(3333, '192.168.1.14');
 }
 bootstrap();
